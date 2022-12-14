a48dad1844776796d636f8a12201fbae1949d542
Ivan <itimoskin790@gmail.com>
2022-12-14 23:53:33 +0300

    Start SASS/SCSS

---
+++ Js/Exercise0-5.js
@@ -0,0 +1,7 @@
+function ExerciseX(x) {
+  if (document.getElementById("exercise1").style.display == "none") {
+    document.getElementById("exercise1").style.display = "block";
+  } else {
+    document.getElementById("exercise1").style.display = "none";
+  }
+}
